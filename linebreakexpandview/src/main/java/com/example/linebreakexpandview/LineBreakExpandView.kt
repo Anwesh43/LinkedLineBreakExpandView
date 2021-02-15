02a9e58eb1472e673ddefcea3f15f6a0f389d214
Anwesh43 <anweshthecool0@gmail.com>
2021-02-16 01:28:56 +0530

    adding Canvas extension methods

--- linebreakexpandview/src/main/java/com/example/linebreakexpandview/LineBreakExpandView.kt
+++ linebreakexpandview/src/main/java/com/example/linebreakexpandview/LineBreakExpandView.kt
@@ -29,3 +29,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLineBreakExpand(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(rot * sf2 * (1f - 2 * j))
+        drawLine(-size * sf1, 0f, size * sf1, 0f, paint)
+        restore()
+    }
+    drawLine(-size * sf3, 0f, size * sf3, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawLBENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBreakExpand(scale, w, h, paint)
+}

15f9977b8f422281c322689519c0578aabfa8d62
Anwesh43 <anweshthecool0@gmail.com>
2021-02-16 01:17:41 +0530

    adding constants and libraries

--- linebreakexpandview/src/main/java/com/example/linebreakexpandview/LineBreakExpandView.kt
+++ linebreakexpandview/src/main/java/com/example/linebreakexpandview/LineBreakExpandView.kt
@@ -1,2 +1,26 @@
 package com.example.linebreakexpandview
 
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#2196F3",
+    "#8BC34A",
+    "#009688",
+    "#3F51B5"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 6.9f
+val delay : Long = 20
+val parts : Int = 3
+val scGap : Float = 0.02F / parts
+val rot : Float = 45f

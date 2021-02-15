33401f5c1c44cc80974445bc3098c6744f99be2e
Anwesh43 <anweshthecool0@gmail.com>
2021-02-16 01:34:13 +0530

    adding LineBreakExpandView class

--- linebreakexpandview/src/main/java/com/example/linebreakexpandview/LineBreakExpandView.kt
+++ linebreakexpandview/src/main/java/com/example/linebreakexpandview/LineBreakExpandView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLBENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBreakExpand(scale, w, h, paint)
 }
+
+class LineBreakExpandView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

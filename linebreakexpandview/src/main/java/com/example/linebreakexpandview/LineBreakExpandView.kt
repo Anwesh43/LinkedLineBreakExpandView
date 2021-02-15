a56ef7e6472a0705572cec9cd0a4ddebaff79444
Anwesh43 <anweshthecool0@gmail.com>
2021-02-16 01:43:34 +0530

    adding LBENode class

--- linebreakexpandview/src/main/java/com/example/linebreakexpandview/LineBreakExpandView.kt
+++ linebreakexpandview/src/main/java/com/example/linebreakexpandview/LineBreakExpandView.kt
@@ -119,4 +119,45 @@ class LineBreakExpandView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBENode(var i : Int, val state : State = State()) {
+
+        private var next : LBENode? = null
+        private var prev : LBENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBENode {
+            var curr : LBENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
